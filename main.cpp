441a9324be768cacbac21220f4212cac07b96c18
Kanayo <kkemaxey@gmail.com>
2025-10-21 20:14:53 -0400

    Updating main file on computer

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
 #include "colors.h"  // Include predefined color codes
 
 // Easy-to-change constants
-const std::string NAME  = "Aren";  // <-- Change this for your name
-const std::string COLOR = RED;     // <-- Change this to BLUE, GREEN, etc. from colors.h
+const std::string NAME  = "Kanayo";  // <-- Change this for your name
+const std::string COLOR = GREEN;     // <-- Change this to BLUE, GREEN, etc. from colors.h
 
 int main() {
-    std::cout << COLOR << "Hello " << NAME << "!" << RESET << std::endl;
+    std::cout << COLOR << "What's good twin! I'm " << NAME << "!" << RESET << std::endl;
     return 0;
 }
